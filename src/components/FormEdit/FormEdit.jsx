4fa98d411cedf7d2f35fd7f2deb7b8bf529bb4b4
MotaAraujo <motaaraujo@outlook.com>
2025-02-24 19:39:56 -0300

    Finalizado

--- src/components/FormEdit/FormEdit.jsx
+++ src/components/FormEdit/FormEdit.jsx
@@ -1,41 +1,89 @@
+import axios from "axios";
 import Input from "../Input/Input.jsx";
+import { useState } from "react";
+import { useNavigate } from "react-router";
 
-export default function FormEdit({ funcionario }) {
+export default function FormEdit({ funcionario, setFormEditIsVisible }) {
+  const router = useNavigate();
+
+  const [name, setName] = useState(funcionario.name);
+  const [email, setEmail] = useState(funcionario.email);
+  const [position, setPosition] = useState(funcionario.position);
+  const [salary, setSalary] = useState(funcionario.salary);
+
+  async function handleEditFuncionario(event) {
+    event.preventDefault();
+
+    const editedFuncionario = {
+      name,
+      email,
+      position,
+      salary,
+    };
+
+    const response = await axios.put(
+      `http://localhost:3001/meusfuncionarios/${funcionario.id}`,
+      editedFuncionario
+    );
+
+    if (response.status === 200) {
+      router(0);
+    }
+  }
+
   return (
     <div>
-      <form className="flex flex-col gap-2">
+      <form
+        className="flex flex-col gap-2 w-[550px]"
+        onSubmit={handleEditFuncionario}
+      >
         <Input
           texto={"Nome"}
           tipo={"text"}
           name={"name"}
-          defaultValue={funcionario.name}
+          defaultValue={name}
+          digitar={(event) => {
+            setName(event.target.value);
+          }}
           placeholder={"Digite o nome do funcionário"}
         />
         <Input
           texto={"Email"}
           tipo={"email"}
           name={"email"}
-          defaultValue={funcionario.email}
+          defaultValue={email}
+          digitar={(event) => {
+            setEmail(event.target.value);
+          }}
           placeholder={"Digite o email do funcionário"}
         />
         <Input
           texto={"Cargo"}
           tipo={"text"}
           name={"position"}
-          defaultValue={funcionario.position}
+          defaultValue={position}
+          digitar={(event) => {
+            setPosition(event.target.value);
+          }}
           placeholder={"Digite o cargo do funcionário"}
         />
         <Input
           texto={"Salário"}
           tipo={"text"}
           name={"salary"}
-          defaultValue={funcionario.salary}
+          defaultValue={salary}
+          digitar={(event) => {
+            setSalary(event.target.value);
+          }}
           placeholder={"Digite o salário do funcionário"}
         />
-        <div className="flex gap-5 items-center justify-between">
+        <div className="flex flex-row gap-5 items-center justify-between mt-5">
           <button
+            onClick={() => {
+              setFormEditIsVisible(false);
+            }}
             type="button"
-            className="text-white bg-gray-800 px-6 py-2 rounded-md hover:bg-gray-600 mr-5 mt-5 w-64"
+            className="text-white bg-gray-800 px-6 py-2 rounded-md hover:bg-gray-600 mr-5 w-64"
           >
             Cancelar Edição
           </button>
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,14 +1,21 @@
-export default function Input({ texto, tipo, placeholder, digitar }) {
+export default function Input({
+  texto,
+  tipo,
+  placeholder,
+  digitar,
+  defaultValue,
+}) {
   return (
     <>
       <label htmlFor={tipo}>{texto}</label>
       <input
-        className="bg-gray-200 py-3 px-2 w-[450px] rounded-lg"
+        className="bg-gray-200 py-3 px-2 w-full rounded-lg"
         type={tipo}
         id={tipo}
         name={tipo}
         placeholder={placeholder}
         onChange={digitar}
+        defaultValue={defaultValue}
       />
     </>
   );
